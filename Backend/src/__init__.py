c37ade6120c5c3e5c40d41307644d6f4014a7d98
juankmiloh <juankmiloh@hotmail.com>
2021-03-03 16:04:42 -0500

    Configuracion de CORS

--- Backend/src/__init__.py
+++ Backend/src/__init__.py
@@ -2,9 +2,9 @@
 import os
 import logging
 from flask import Flask
-from flask_cors import CORS
-from flask_injector import FlaskInjector
 from flask_sqlalchemy import SQLAlchemy
+from flask_injector import FlaskInjector
+from flask_cors import CORS
 from injector import Module, Injector, singleton
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -43,14 +43,10 @@ def create_app():
     from .controller import controller as controller_blueprint
     app.register_blueprint(controller_blueprint)
 
-    if app.config.get("ENV") == 'development':
+    if app.config.get("ENV") == 'development' or app.config.get("ENV") == 'production':
         CORS(app, resources={
              r"*": {"origins": "*", "supports_credentials": True}})
 
     FlaskInjector(app=app, injector=injector)
-
-    # print("____________________")
-    # print(app.config.get("ENV"))
-    # print("____________________")
 
     return app
