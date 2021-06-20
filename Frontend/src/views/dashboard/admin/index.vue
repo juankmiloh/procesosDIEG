88872687fe162ccfc40ce4dbdc0e4ccbc66a274e
juankmiloh <juankmiloh@hotmail.com>
2021-06-20 16:25:06 -0500

    Actualizando rama develop

--- Frontend/src/views/dashboard/admin/index.vue
+++ Frontend/src/views/dashboard/admin/index.vue
@@ -5,7 +5,6 @@
         <aside>
           <span class="text-header">
             <b>{{ datosDependencia.nombre | uppercase }}</b>
-            <h2>Rama de desarrollo</h2>
           </span>
         </aside>
       </el-col>
